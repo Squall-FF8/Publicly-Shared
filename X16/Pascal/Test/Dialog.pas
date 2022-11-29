d47763bc679875d0712deafe0c2ae745fd41d35e
Squall-FF8 <35965968+Squall-FF8@users.noreply.github.com>
2022-11-29 21:40:38 +0200

    Added 2bpp font for tests

--- X16/Pascal/Test/Dialog.pas
+++ X16/Pascal/Test/Dialog.pas
@@ -4,6 +4,9 @@
 program NewProgram;
 uses x16, VERA, Window;
 
+var
+  i, X: byte;
+
 begin
   vControl := 0;  // VERA_DATA1 will be used. DC_SEL= 0
 
@@ -14,14 +17,20 @@ begin
   winRectangle(1, 1, 190, 126);  
   winRectangle(10, 10, 172, 108);
   
+  dSetPos($10, X, 30);
+  TestText;
   //_X := 80;
   //_Y := 80;
-  dSetPos($10, 25, 30);
-  winCharOut(1);
-  dSetPos($10, 30, 30);
-  winCharOut(0);
-  dSetPos($10, 37, 30);
-  winCharOut(2);
+{  X := 10;
+  for i := 16 to 31 do
+    dSetPos($10, X, 30);
+    winCharOut(32+i);
+    X := X + 6;
+  end; } 
+  //dSetPos($10, 30, 30);
+  //winCharOut(34);
+  //dSetPos($10, 37, 30);
+  //winCharOut(35);
 
   VERA_DC_VIDEO :=  VERA_DC_VIDEO or (1 << 6);  // Enable Sprites
 
