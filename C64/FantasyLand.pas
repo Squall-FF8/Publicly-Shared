5bc908be130755f5c7d6f2bd9095fede7716d3e8
Squall-FF8 <35965968+Squall-FF8@users.noreply.github.com>
2022-12-03 01:57:14 +0200

    C64 content moved to separate folder

--- C64/FantasyLand.pas
+++ C64/FantasyLand.pas
@@ -1,7 +1,10 @@
-////////////////////////////////////////////
-// New program created in 12/2/2022}
-////////////////////////////////////////////
-program PETSCII_ART;
+{ ==================================
+  Demo program - PETSCII Art (c64)
+  Created: 12/1/2022
+  Artwork: Dr. TerrorZ, 2017
+  URL:     https://csdb.dk/release/?id=159911
+   ================================== }
+program FantasyLand;
 uses Commodore64;
 
 var
@@ -10,8 +13,8 @@ var
   BorderColor     : byte absolute $D020;
   BackgroundColor : byte absolute $D021;
   
-  Characters : array of byte = ({$BIN2CSV chars.bin});
-  Colors     : array of byte = ({$BIN2CSV colors.bin});
+  Characters : array of byte = ({$BIN2CSV FantasyLand.chr});
+  Colors     : array of byte = ({$BIN2CSV FantasyLand.col});
   
   vram_Char  : array[1000] of byte absolute $400;
   vram_Color : array[1000] of byte absolute $D800;
@@ -19,8 +22,8 @@ var
   i: word;
   
 begin
-  ScreenControl   := $0B;
-  MemorySetup     := $14; 
+  //ScreenControl   := $0B;
+  //MemorySetup     := $14; 
   BorderColor     := $00;
   BackgroundColor := $00;
   
