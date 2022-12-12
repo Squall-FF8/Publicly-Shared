12257a772d3393b153d949a7d0751b8b3d22d784
Squall-FF8 <35965968+Squall-FF8@users.noreply.github.com>
2022-12-13 00:15:40 +0200

    used $STRING directive for null-terminated

--- X16/Pascal/Test/Test_Pointers.pas
+++ X16/Pascal/Test/Test_Pointers.pas
@@ -3,106 +3,116 @@
 ////////////////////////////////////////////
 program TestPointers;
 uses X16, Text;
+{$STRING NULL_TERMINATED}
 
   procedure Pass;
   begin
-    Print(@' PASS'#13#0);
+    Print(@' PASS'#13);
   end; 
 
   procedure Fail;
   begin
-    Print(@' FAIL'#13#0);
+    Print(@' FAIL'#13);
   end; 
 
 
 var
   b: byte;
   w: word;
-  pb: ^byte;
-  pw: ^word;
+  pb, pb1: ^byte;
+  pw, pw1: ^word;
+  p, q: pointer;
   
 begin
   b := $10;
   w := $1234;
 
   pb := word($10);
-  Print(@'ASSIGN <256 CONST ADDRESS:'#0);
+  Print(@'ASSIGN <256 CONST ADDRESS:');
   if pb = word($10) then Pass; else Fail end;  
 
   pb := $1012;
-  Print(@'ASSIGN >256 CONST ADDRESS:'#0);
+  Print(@'ASSIGN >256 CONST ADDRESS:');
   if pb = $1012 then Pass; else Fail end;  
 
   w := $20;
   pb := w;
-  Print(@'ASSIGN <256 WORD VAR ADDRESS:'#0);
+  Print(@'ASSIGN <256 WORD VAR ADDRESS:');
   if pb = word($20) then Pass; else Fail end;  
 
   w := $2030;
   pb := w;
-  Print(@'ASSIGN >256 WORD VAR ADDRESS:'#0);
+  Print(@'ASSIGN >256 WORD VAR ADDRESS:');
   if pb = word($2030) then Pass; else Fail end;  
 
   pb := @b;
-  Print(@'POINTS TO BYTE VARIABLE:'#0);
+  Print(@'POINTS TO BYTE VARIABLE:');
   if pb^ = b then Pass; else Fail end;  
 
   pw := @w;
-  Print(@'POINTS TO WORD VARIABLE:'#0);
+  Print(@'POINTS TO WORD VARIABLE:');
   if pw^ = w then Pass; else Fail end;  
 
   pb^ := $10;
-  Print(@'ASSIGN A CONST BYTE TO BYTE PTR:'#0);
+  Print(@'ASSIGN A CONST BYTE TO BYTE PTR:');
   if pb^ = $10 then Pass; else Fail end;  
 
   pb^ := b;
-  Print(@'ASSIGN A VAR BYTE TO BYTE PTR:'#0);
+  Print(@'ASSIGN A VAR BYTE TO BYTE PTR:');
   if pb^ = b then Pass; else Fail end;  
 
   pw^ := word($10);
-  Print(@'ASSIGN A CONST BYTE TO WORD PTR:'#0);
+  Print(@'ASSIGN A CONST BYTE TO WORD PTR:');
   if pw^ = $10 then Pass; else Fail end;  
 
   pw^ := $1011;
-  Print(@'ASSIGN A CONST WORD TO WORD PTR:'#0);
+  Print(@'ASSIGN A CONST WORD TO WORD PTR:');
   if pw^ = $1011 then Pass; else Fail end;  
 
   pw^ := w;
-  Print(@'ASSIGN A VAR WORD TO WORD PTR:'#0);
+  Print(@'ASSIGN A VAR WORD TO WORD PTR:');
   if pw^ = w then Pass; else Fail end;  
 
   pb := $1000;
   pb := pb + $345;
-  Print(@'ADDITION WITH A CONST:'#0);
+  Print(@'ADDITION WITH A CONST:');
   if pb = $1345 then Pass; else Fail end;  
 
   pb := $1000;
   pb := pb + b;
-  Print(@'ADDITION WITH A VAR:'#0);
+  Print(@'ADDITION WITH A VAR:');
   if pb = $1010 then Pass; else Fail end;  
 
   //pb := $1345;
   //pb := pb - $345;
-  //Print(@'SUBSTRACTION WITH A CONST:'#0);
+  //Print(@'SUBSTRACTION WITH A CONST:');
   //if pb = $1000 then Pass; else Fail end;  
 
   //pb := $1010;
   //pb := pb - b;
-  //Print(@'SUBSTRACTION WITH A VAR:'#0);
+  //Print(@'SUBSTRACTION WITH A VAR:');
   //if pb = $1010 then Pass; else Fail end;
   
-  //pb := $1234;
-  //Print(@'SUBSTRACTION WITH A CONST:'#0);
-  //if pb <= $1234 then Pass; else Fail end;  
+  pb := $1234; pb1 := $2000;
+  Print(@'COMPARE BYTE PTR:');
+  if pb <= pb1 then Pass; else Fail end;  
+    
+  pw := $1234; pw1 := $2000;
+  Print(@'COMPARE WORD PTR:');
+  if pw <= pw1 then Pass; else Fail end;
+
+  p := $1234; q := $2000;
+  Print(@'COMPARE WORD PTR:');
+  if p <= q then Pass; else Fail end;
     
   pb := $FF0;
   pb += $10;
-  Print(@'+= WITH A CONST:'#0);
+  Print(@'+= WITH A CONST:');
   if pb = $1000 then Pass; else Fail end;  
 
   pb := $FF0;
   pb += b;
-  Print(@'+= WITH A VARIABLE:'#0);
+  Print(@'+= WITH A VARIABLE:');
   if pb = $1000 then Pass; else Fail end;  
 
   {$RTS} 
