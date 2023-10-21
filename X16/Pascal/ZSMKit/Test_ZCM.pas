fcf0ddd9aded0e732743e76cb9e9559d62a71a9a
Squall-FF8 <35965968+Squall-FF8@users.noreply.github.com>
2023-10-21 13:45:23 +0300

    Implemented the whole ZSMKit library
    Two examples of playing ZSM and ZCM.

--- X16/Pascal/ZSMKit/Test_ZCM.pas
+++ X16/Pascal/ZSMKit/Test_ZCM.pas
@@ -1,28 +1,29 @@
 ////////////////////////////////////////////
 // New program created in 10/19/2023}
 ////////////////////////////////////////////
-program Test_ZSMKit;
+program Test_ZCM;
 uses X16, ZSMKit;
 
 const
   ZSM_BANK = 2;  // ZSMKit workspace bank
   ZSM_DATA = 3;  // Song data
-  Priority = 0;
+  Slot = 0;
+  Volume = 10;
 
 var
   ZSM_Code: array[] of char = 'ZSMKIT8C00.BIN';
-  ZSM_Song: array[] of char = 'MUSIC.ZSM';
+  ZCM_Song: array[] of char = 'TEST.ZCM';
   
 begin
   LoadFileInRAM(@ZSM_Code, ZSM_Code.Length, ZSM_BASE);
-  LoadFileInBRAM(@ZSM_Song, ZSM_Song.Length, ZSM_DATA);
+  LoadFileInBRAM(@ZCM_Song, ZCM_Song.Length, ZSM_DATA);
 
   zsm_InitEngine(ZSM_BANK);
   zsm_SetISR;
   
   RAM_BANK := ZSM_DATA;
-  zsm_SetMem(Priority, RAM_WIN.Low, RAM_WIN.High);
+  zcm_SetMem(Slot, RAM_WIN.Low, RAM_WIN.High);
   
-  zsm_Play(Priority);
+  zcm_Play(Slot, Volume);
   {$RTS} 
 end.
--- X16/Pascal/lib/ZSMKit.pas
+++ X16/Pascal/lib/ZSMKit.pas
@@ -36,29 +36,154 @@ const
   __zcm_stop          = ZSM_BASE + $51;
   __zsmkit_setisr     = ZSM_BASE + $54;
   __zsmkit_clearisr   = ZSM_BASE + $57;
+
   
-{ INPUT: A = designated RAM bank to use for ZSMKit engine state
+{ INPUT:  A = designated RAM bank to use for ZSMKit engine state
   Initializes the memory locations used by ZSMKit's engine and calls
   audio_init using the X16's AUDIO API to reset its state..
   Needs to be called once before any other ZSMKit function. }  
   procedure zsm_InitEngine(Bank: byte registerA);
   
-{ INPUT: X = priority
+{ INPUT:  A = 0 advance music data and PCM
+          A = 1 advance PCM only
+          A = 2 advance music data only
+  advances songs and plays the notes}  
+  procedure zsm_Tick(Flag: byte registerA);
+  
+{ INPUT:  X = Slot
   Sets up the song to start playing back on the next tick if
   the song is valid and ready to play }  
-  procedure zsm_Play(Priority: byte registerX);
+  procedure zsm_Play(Slot: byte registerX);
   
-{ INPUT:  X = priority
-          A/Y = data pointer,
-          $00 = ram bank (bdefore calling the routine!)
-  Sets the start of memory for the priority, reads it,
-  and parses the header
+{ INPUT:  X = Slot
+  Pauses playback of a song. Playback can optionally be resumed from that point later with zsm_play. }  
+  procedure zsm_Stop(Slot: byte registerX);
+
+{ INPUT:  X = Slot
+  Stops playback of a song (if it is already playing) and resets its pointer to the beginning of the song.
+  Playback can then be started again with zsm_play.}  
+  procedure zsm_Rewind(Slot: byte registerX);
+  
+{ INPUT:  X = Slot
+  Stop and closes streaming songs, sets slot to unused/not playable.}  
+  procedure zsm_Close(Slot: byte registerX);
+
+{ If the slot has a song being streamed from a file, top the buffer off if appropriate.
+  Must be called from the main loop since it deals with file I/O}  
+  procedure zsm_FillBuffers;
+
+{ INPUT:  X = Slot (0..3)
+          A = LFN/SA logical file number/secondary address
+          Y = IEC device
+  Sets the logical file number, secondary address, and IEC device for a particular slot }
+  procedure zsm_SetLfs(Slot: byte registerX; LFN: byte registerA; Device: byte registerY);
+  
+{ INPUT:  X = Slot (0..3)
+          A/Y = pointer (lo hi) in low RAM to null-terminated filename
+  This is an alternate song-loading method. It sets up a slot to stream a ZSM file from disk (SD card). 
+  Sets the filename for the slot, opens it, and parses the header.
   Must only be called from main loop routines. }  
-  procedure zsm_SetMem(Priority: byte registerX; DataLo: byte registerA; DataHi: byte registerY); 
+  procedure zsm_SetFile(Slot: byte registerX; DataLo: byte registerA; DataHi: byte registerY); 
+
+{ INPUT:  X = Slot (0..3)
+          A/Y = load address (lo hi)
+          $00 = RAM bank (bdefore calling the routine!)
+  OUTPUT: A/Y = next memory location after end of load, 
+          $00 = RAM bank
+  For streamed ZSM files that have PCM data, this routine can be used to load the PCM data into memory at the specified memory location.
+  Sets the start of memory for the slot, reads it, and parses the header.
+  Must only be called from main loop routines. }  
+  procedure zsm_LoadPCM(Slot: byte registerX; DataLo: byte registerA; DataHi: byte registerY); 
   
+{ INPUT:  X = Slot (0..3)
+          A/Y = data pointer (lo hi)
+          $00 = ram bank (bdefore calling the routine!)
+  Sets the start of memory for the slot, reads it, and parses the header
+  Must only be called from main loop routines. }  
+  procedure zsm_SetMem(Slot: byte registerX; DataLo: byte registerA; DataHi: byte registerY); 
+
+{ INPUT:  X = Slot
+          A = Attenuation value $00: full volume .. $3F: full mute.
+  Changes the master volume of a priority slot by setting an attenuation value. }  
+  procedure zsm_SetAtten(Slot: byte registerX; Atten: byte registerA);
+
+{ INPUT:  X = Slot (0..3)
+          A/Y = pointer to callback (lo hi)
+          $00 = ram bank (bdefore calling the routine!)
+  Sets up a callback address. The callback is triggered whenever a song loops 
+  or ends on its own, or a synchronization message is processed in the ZSM data. }  
+  procedure zsm_SetCallback(Slot: byte registerX; AddressLo: byte registerA; AddressHi: byte registerY); 
+
+{ INPUT:  X = Slot (0..3)
+  Clears the callback assigned to the slot. }  
+  procedure zsm_ClearCallback(Slot: byte registerX); 
+
+{ INPUT:  X = Slot (0..3)
+  OUTPUT: C = Is playing 1:Yes, 0:No
+          Z = Is available 1: No, 0: Yes
+          A/Y = Loop counter (lo hi) - the number of times the song has looped. 
+  Returns the playback state of a priority slot. }  
+  procedure zsm_GetState(Slot: byte registerX);
+
+{ INPUT:  X = Slot (0..3)
+          A/Y = tick rate (lo hi)
+  Sets the current tick rate of the song. }  
+  procedure zsm_SetRate(Slot: byte registerX; RateLo: byte registerA; RateHi: byte registerY); 
+
+{ INPUT:  X = Slot (0..3)
+  OUTPUT: A/Y = tick rate (lo hi)  !!! THIS NEED PASCAL RETURN!!!
+  Returns the current tick rate of the song. }  
+  procedure zsm_GetRate(Slot: byte registerX); 
+
+{ INPUT:  X = Slot (0..3)
+          C = whether to loop 1:Yes, 0:No
+  If carry is set, enable the looping behaivor. If carry is clear, disable looping. }  
+  procedure zsm_SetLoop(Slot: byte registerX); 
+
+{ INPUT:  X = Slot (0..3)
+          Y = channel (YM)
+          A = Attenuation value $00:full volume .. $3F:full mute.
+  Changes the volume of an individual OPM channel for a priority slot by setting an attenuation value. }  
+  procedure zsm_OpmAtten(Slot: byte registerX; Channel: byte registerY; Atten: byte registerA); 
+
+{ INPUT:  X = Slot (0..3)
+          Y = channel (PSG)
+          A = Attenuation value $00:full volume .. $3F:full mute.
+  Changes the volume of an individual PSG channel for a priority slot by setting an attenuation value. }  
+  procedure zsm_PsgAtten(Slot: byte registerX; Channel: byte registerY; Atten: byte registerA); 
+
+{ INPUT:  X = Slot (0..3)
+          A = Attenuation value $00:full volume .. $3F:full mute.
+  Changes the volume of PCM by setting an attenuation value. }  
+  procedure zsm_PcmAtten(Slot: byte registerX; Atten: byte registerA); 
+
+{ INPUT:  A = new rate (integer portion in Hz)
+          Y = new rate (fractional portion in 1/256th Hz)
+  Sets a new global interrupt rate in Hz. }  
+  procedure zsm_SetInterruptRate(RateLo: byte registerA; RateHi: byte registerY); 
+
+
+{ INPUT:  X = Slot (0 .. 31)
+          A/Y = data pointer (lo hi)
+          $00 = ram bank (bdefore calling the routine!)
+  Sets the start of memory for a digital sample (ZCM format) }  
+  procedure zcm_SetMem(Slot: byte registerX; DataLo: byte registerA; DataHi: byte registerY); 
+
+{ INPUT:  X = Slot (0 .. 31)
+          A = Volume (1 .. 15)
+  Begins playback of a ZCM digital sample }  
+  procedure zcm_Play(Slot: byte registerX; Volume: byte registerA);
+
+{ Stops playback of a ZCM if one is playing
+  Does not stop the PCM channel if a ZSM's PCM event is playing }
+  procedure zcm_Stop;
+
 
 { Sets up a default ISR handler and injects it before the existing ISR }  
   procedure zsm_SetISR;
+
+{ Clears the default ISR handler if it exists and restores the previous one }  
+  procedure zsm_ClearISR;
 
   
 implementation
@@ -71,6 +196,178 @@ begin
   end 
 end; 
 
+procedure zsm_Tick(Flag: byte registerA);
+begin
+  asm 
+    JSR __zsm_tick
+  end 
+end; 
+
+procedure zsm_Play(Slot: byte registerX);
+begin
+  asm 
+    JSR __zsm_play
+  end 
+end; 
+
+procedure zsm_Stop(Slot: byte registerX);
+begin
+  asm 
+    JSR __zsm_stop
+  end 
+end; 
+
+procedure zsm_Rewind(Slot: byte registerX);
+begin
+  asm 
+    JSR __zsm_rewind
+  end 
+end; 
+
+procedure zsm_Close(Slot: byte registerX);
+begin
+  asm 
+    JSR __zsm_close
+  end 
+end;
+
+procedure zsm_FillBuffers; 
+begin
+  asm 
+    JSR __zsm_fill_buffers
+  end 
+end;
+
+procedure zsm_SetLfs(Slot: byte registerX; LFN: byte registerA; Device: byte registerY);
+begin
+  asm 
+    JSR __zsm_setlfs
+  end 
+end;
+
+procedure zsm_SetFile(Slot: byte registerX; DataLo: byte registerA; DataHi: byte registerY); 
+begin
+  asm 
+    JSR __zsm_setfile
+  end 
+end;
+
+procedure zsm_LoadPCM(Slot: byte registerX; DataLo: byte registerA; DataHi: byte registerY); 
+begin
+  asm 
+    JSR __zsm_loadpcm
+  end 
+end;
+
+procedure zsm_SetMem(Slot: byte registerX; DataLo: byte registerA; DataHi: byte registerY); 
+begin
+  asm 
+    JSR __zsm_setmem
+  end 
+end; 
+
+procedure zsm_SetAtten(Slot: byte registerX; Atten: byte registerA);
+begin
+  asm 
+    JSR __zsm_setatten
+  end 
+end; 
+
+procedure zsm_SetCallback(Slot: byte registerX; AddressLo: byte registerA; AddressHi: byte registerY); 
+begin
+  asm 
+    JSR __zsm_setcb
+  end 
+end;
+
+procedure zsm_ClearCallback(Slot: byte registerX); 
+begin
+  asm 
+    JSR __zsm_clearcb
+  end 
+end;
+
+procedure zsm_GetState(Slot: byte registerX);
+begin
+  asm 
+    JSR __zsm_getstate
+  end 
+end;
+
+procedure zsm_SetRate(Slot: byte registerX; RateLo: byte registerA; RateHi: byte registerY); 
+begin
+  asm 
+    JSR __zsm_setrate
+  end 
+end;
+
+procedure zsm_GetRate(Slot: byte registerX); 
+begin
+  asm 
+    JSR __zsm_getrate
+  end 
+end;
+
+procedure zsm_SetLoop(Slot: byte registerX); 
+begin
+  asm 
+    JSR   __zsm_setloop
+  end 
+end;
+
+procedure zsm_OpmAtten(Slot: byte registerX; Channel: byte registerY; Atten: byte registerA); 
+begin
+  asm 
+    JSR   __zsm_opmatten
+  end 
+end;
+
+procedure zsm_PsgAtten(Slot: byte registerX; Channel: byte registerY; Atten: byte registerA); 
+begin
+  asm 
+    JSR   __zsm_psgatten
+  end 
+end;
+
+procedure zsm_PcmAtten(Slot: byte registerX; Atten: byte registerA); 
+begin
+  asm 
+    JSR   __zsm_pcmatten
+  end 
+end;
+
+procedure zsm_SetInterruptRate(RateLo: byte registerA; RateHi: byte registerY); 
+begin
+  asm 
+    JSR   __zsm_set_int_rate
+  end 
+end;
+
+
+
+procedure zcm_SetMem(Slot: byte registerX; DataLo: byte registerA; DataHi: byte registerY); 
+begin
+  asm 
+    JSR __zcm_setmem
+  end 
+end; 
+
+procedure zcm_Play(Slot: byte registerX; Volume: byte registerA);
+begin
+  asm 
+    JSR __zcm_play
+  end 
+end; 
+
+procedure zcm_Stop;
+begin
+  asm 
+    JSR __zcm_stop
+  end 
+end; 
+  
+
+
 procedure zsm_SetISR;  
 begin
   asm 
@@ -78,20 +375,11 @@ begin
   end 
 end; 
 
-procedure zsm_SetMem(Priority: byte registerX; DataLo: byte registerA; DataHi: byte registerY); 
+procedure zsm_ClearISR;
 begin
   asm 
-    JSR __zsm_setmem
+    JSR __zsmkit_clearisr
   end 
 end; 
 
-procedure zsm_Play(Priority: byte registerX);
-begin
-  asm 
-    JSR __zsm_play
-  end 
-end; 
-
-
-  
 end.
