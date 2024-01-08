d7140e3e749f697c10ecc1dac5a596f2b1d356ab
Squall-FF8 <35965968+Squall-FF8@users.noreply.github.com>
2024-01-08 15:13:56 +0200

    latest changes

--- X16/Pascal/Test/Test_X16.pas
+++ X16/Pascal/Test/Test_X16.pas
@@ -1,43 +1,20 @@
 program Test;
-uses X16, VERA, Utils, Text;
+uses X16, VERA, VeraFX, Utils, Text;
 
 var
   b: byte;
   w, w1, w2: word;
   d, d1: dword;
-  
-  procedure PrintD;
-  begin
-    NewLine;
-    Print_Hex32((@d).High, (@d).Low);
-  end;
-   
-begin
-  w := $1122;
-  //d := $11223344;
-  d := $100;
-  d1 := 0;
-  //PrintD;
-  //d1 := d;
-  d := d + w;
-  PrintD;
-  d := d1 + $44332211;
-  PrintD;
-  d := d + d1;
-  PrintD;
 
-  //w := 8191;
-  //w2 := 8187;
-  //w1 := w mod w2; 
-  //w1 := w mod word(8188);
-  //HexToDec65535(w1);
-  //HexToDec65535(w mod word(8188));
-  //HexToDec65535(word(12345));
-  //Print_Dec8z(123);
-{  w := 32768;
-  w1 := w div word(8192);
-  w2 := 32768 div w1;
-  Print_Dec16z(32768 div w1);
-}  
+  v: [5]char;
+  t, t1: triplet;
+  
+begin
+  d := d1 + 1 + 1;
+  //t := triplet(d);
+  //t := t1;
+  //t := triplet($34121212);
+  //t := triplet($123456);
+  
   {$RTS} 
 END.  
